a63f1e452e1663f5b17092d9d4976d975ac0cda5
WB <biski@biski-VirtualBox.(none)>
2013-12-14 13:02:28 +0100

    Dodane bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Poodle\TestBundle\TestBundle(),
+            new Poodle\UserBundle\UserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Poodle/TestBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Poodle\TestBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('TestBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
